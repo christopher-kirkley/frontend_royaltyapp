65f2e522afc617ca1aaf2a5abf3c0d26de373160
Christopher Kirkley <ckirkley@gmail.com>
2020-07-15 23:19:33 -0700

    Restyled Income page.

--- src/income/AddStatementForm.js
+++ src/income/AddStatementForm.js
@@ -8,6 +8,10 @@ import TextField from '@material-ui/core/TextField'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Select from '@material-ui/core/Select'
+import Typography from '@material-ui/core/Typography'
+import MenuItem from '@material-ui/core/MenuItem'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
 
 function AddStatementForm() {
 
@@ -21,30 +25,40 @@ function AddStatementForm() {
 
 		
 	return (
-		<Container>
-		<h2>Upload Income Statement</h2>
-		<p>This is a place to upload CSV of your income statement.</p>
-		<Grid container xs={12} spacing={4}>
-			<form onSubmit={handleUpload} id="form">
-			<Grid item>
-				<TextField id="catalog_to_upload" name="upload" type="file"/>	
+		<Container component="main" maxWidth="xs" spacing={4}>
+		<div style={{marginTop: 10, display: "flex", flexDirection: "column",
+								alignItems:"center"}}>
+			<Typography component="h2" variant="h5">
+				Upload Income Statement
+			</Typography>
+			<form onSubmit={handleUpload} id="form" style={{marginTop: 10, width: '100%'}}>
+			<Grid container  spacing={4}> 
+			<Grid item xs={12}>
+				<TextField fullWidth id="catalog_to_upload" name="upload" type="file"/>	
 			</Grid>
-			<Grid item>
+			<Grid item xs={12}>
+				<FormControl fullWidth>
+				<InputLabel>Statement Source</InputLabel>
 				<Select>
-					<option>Bandcamp</option>
+		          <MenuItem value={10}>Bandcamp</MenuItem>
+		          <MenuItem value={10}>SD</MenuItem>
+		          <MenuItem value={10}>SD Digital</MenuItem>
 				</Select>
+				</FormControl>
 			</Grid>
-			<Grid item>
+			<Grid item xs={12}>
 				<Button
 					variant="contained"
 					color="primary"
 					id="catalog_upload"
 					name="submit"
 					type="submit"
+					fullWidth
 				>Upload</Button>
 			</Grid>
-			</form>
 		</Grid>
+		</form>
+		</div>
 		</Container>
 	)
 }
