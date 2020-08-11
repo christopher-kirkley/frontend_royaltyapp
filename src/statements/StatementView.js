1b913f023223d1587e1509fac37a75632f6d8ca7
Christopher Kirkley <ckirkley@gmail.com>
2020-08-11 12:56:18 -0700

    Added generated statement module.

--- src/statements/StatementView.js
+++ src/statements/StatementView.js
@@ -4,11 +4,15 @@ import { Redirect } from 'react-router-dom'
 import { useHistory } from "react-router-dom";
 
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
 	  MuiPickersUtilsProvider,
@@ -23,10 +27,71 @@ function StatementView() {
 	const history = useHistory()
 
 	const [msg, setMsg] = useState([]);
+	const [statements, setStatements] = useState([]);
+
+	useEffect(() => {
+		fetch('http://localhost:5000/statements/view')
+		.then(res => res.json())
+		.then(json => setStatements(json))
+		.catch(res => setMsg('Error fetching data'))
+	}, [])
+
+	function handleClick(e) {
+		const id = e.currentTarget.value
+		history.push(`/expense/${id}`)
+	}
+
+	function handleDelete(e) {
+		const id = e.currentTarget.value
+		fetch(`http://localhost:5000/expense/statements/${id}`, {
+			method: 'DELETE'
+		})
+		}
 
 	return (
 			<Container>
 				<Header name='View Statements'/>
+				<Table id="statement_table">
+					<TableRow>
+						<TableCell>
+						Statement Name
+						</TableCell>
+						<TableCell/>
+					</TableRow>
+				{ statements.map((row) => 
+					<TableRow>
+							<TableCell>
+								{row.statement_name}
+							</TableCell>
+							<TableCell>
+							<Button
+								variant="contained"
+								color="primary"
+								id={row.id}
+								value={row.id}
+								name="submit"
+								type="submit"
+								fullWidth
+								onClick={handleClick}
+								>
+								View
+								</Button>
+							</TableCell>
+							<TableCell>
+							<Button
+								variant="contained"
+								color="secondary"
+								id="delete"
+								value={row.id}
+								onClick={handleDelete}
+								fullWidth
+								>
+								Delete
+								</Button>
+							</TableCell>
+					</TableRow>
+				)}
+				</Table>
 			</Container>
 		)
 	}
