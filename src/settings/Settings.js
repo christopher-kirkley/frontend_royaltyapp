ec5d054e5a02b1b1f5b08612d0c35a96fda36ff8
Christopher Kirkley <ckirkley@gmail.com>
2020-10-18 15:19:52 -0700

    Add income distributor setting menu.

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -9,6 +9,8 @@ import {
 
 import { useHistory } from "react-router-dom";
 
+import { useForm, Controller } from 'react-hook-form'
+
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
@@ -16,6 +18,9 @@ import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import TextField from '@material-ui/core/TextField'
+import Divider from '@material-ui/core/Divider'
+import NativeSelect from '@material-ui/core/NativeSelect'
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -25,11 +30,13 @@ const useStyles = makeStyles(theme => ({
 	},
 }))
 
-function Artist() {
+function Settings() {
 
 	const classes = useStyles()
 
 	const history = useHistory();
+
+	const { handleSubmit, control, setValue } = useForm()
 
 	function handleClick() {
 	}
@@ -40,6 +47,70 @@ function Artist() {
 		<Grid container direction="row" >
 			<Grid item xs={12}>
 				<Paper className={classes.paper}> 
+					<Grid container alignItems="center">
+						<Grid item xs={2} >
+							<Typography color="textSecondary"
+								component="h6" variant="caption" align="left"
+								gutterBottom>INCOME SETTINGS</Typography>
+						<Divider style={{marginTop: 10, marginBottom: 10}}/>
+						</Grid>
+						<Grid container>
+							<Grid xs={3}>
+								<Typography variant="subtitle1">
+								Distributor
+								</Typography>
+							</Grid>
+							<Grid>
+								<Controller
+									as={
+										<NativeSelect>
+											<option>Bandcamp</option>
+											<option>Secretly Physical</option>
+											<option>Secretly Digital</option>
+										</NativeSelect>
+									}
+									name="distributor"
+									id="distributor"
+									defaultValue=""
+									control={control}
+								/>
+							</Grid>
+						</Grid>
+						<Grid container>
+							<Grid xs={3}>
+								<Typography variant="subtitle1">
+								Fee per order
+								</Typography>
+							</Grid>
+							<Grid xs={2}>
+								<Controller
+									as={TextField}
+									name="order_fee"
+									id="order_fee"
+									defaultValue=""
+									control={control}
+								/>
+							</Grid>
+						</Grid>
+						<Grid container>
+							<Grid xs={3}>
+								<Typography variant="subtitle1">
+								Percentage per Order
+								</Typography>
+							</Grid>
+							<Grid xs={2}>
+								<Controller
+									as={TextField}
+									name="order_percentage"
+									id="order_percentage"
+									defaultValue=""
+									control={control}
+								/>
+							</Grid>
+						</Grid>
+				</Grid>
+				
+
 				</Paper>
 			</Grid>
 		</Grid>
@@ -48,4 +119,4 @@ function Artist() {
 }
 
 
-export default Artist;
+export default Settings;
